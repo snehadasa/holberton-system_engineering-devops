3504ca9be5c6ab6074d0955d2d503a6e3a26530b
snehadasa <sneha.dasasneha@gmail.com>
2019-08-05 16:12:02 -0700

    indentation

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Using Puppet, create a manifest that kills a process named killmenow.
 exec { 'killmenow':
-path     => '/usr/bin',
-command  => 'pkill -f killmenow',
+path    => '/usr/bin',
+command => 'pkill -f killmenow',
 }
