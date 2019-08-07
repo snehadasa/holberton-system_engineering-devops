fc4a1db66537a0ffa0110adf33a7819584c68f78
snehadasa <sneha.dasasneha@gmail.com>
2019-08-06 20:36:09 -0700

    edit

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,11 +2,11 @@
 file_line { 'Turn off passwd auth':
 ensure => 'present',
 path   => '/etc/ssh/ssh_config',
-line   => 'IdentifyFile ~/.ssh/holberton',
+line   => 'PasswordAuthentification no',
 }
 
 file_line { 'Declare identify file':
 ensure => 'present',
 path   => '/etc/ssh/ssh_config',
-line   => 'PasswordAuthentification no',
+line   => 'IdentifyFile ~/.ssh/holberton',
 }
