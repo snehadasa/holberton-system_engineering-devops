5b6a2ee44a2e9455ed876a6240602d24328b940e
snehadasa <sneha.dasasneha@gmail.com>
2019-09-16 18:44:29 +0000

    export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,2 +1,28 @@
 #!/usr/bin/python3
-"""Export to JSON"""
+"""Gather data from an API"""
+
+import urllib
+import requests
+from sys import argv
+import json
+
+
+if __name__=="__main__":
+    """Python script that, using this REST API, for a given employee ID, returns
+       information about his/her TODO list progress."""
+
+    u_id = argv[1]
+    user_request = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                           .format(u_id)).json()
+    name = user_request.get('name')
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos").json()
+
+    tasks = []
+    for task in todo:
+        dic = {"task": task.get('title'), "completed": task.get('completed'),
+               "username": name}
+        tasks.append(dic)
+        json_f = {}
+        json_f[u_id] = tasks
+    with open("{}.json".format(u_id), mode='w') as file:
+        json.dump(json_f, file)
