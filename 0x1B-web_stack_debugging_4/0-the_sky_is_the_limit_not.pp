45ec680d9742ae6b9c3418b0ebdf596cc12655eb
snehadasa <sneha.dasasneha@gmail.com>
2019-10-10 16:57:01 +0000

    change to 4096

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 #debugging apache server
-exec { 'increase request limit':
-  command => "sed -i 's/15/4096/g' /etc/default/nginx ",
-  path    => '/bin',
+exec { 'increaserequestlimit':
+  command => 'sed -i "s/15/4096/g" /etc/default/nginx && service nginx restart',
+  path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin']
 }
